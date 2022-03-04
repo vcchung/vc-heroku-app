94b5c4fc6cc8b0f7f252e7a3d198c75033719f96
vcchung <vcchung@gmail.com>
2022-03-04 14:55:14 +0800

    customize for heroku

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
+const PORT =process.env.PORT || 3000
+
 const app=express()
 app.get('/', (request, response)=>{
   response.send('This is message sent from Vincent!');
 })
 
-app.listen(3000, ()=>{
-    console.log("Express is listening in port 3000")
+app.listen(PORT, ()=>{
+    console.log(`Express is listening in port ${PORT}`)
 })
