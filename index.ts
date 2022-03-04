07a51e7c8bfad4a9d4e26e1d6bccb48c71643076
vcchung <vcchung@gmail.com>
2022-03-04 18:11:06 +0800

    add eslint and prettier

--- index.ts
+++ index.ts
@@ -1,22 +1,22 @@
-import express from 'express';
+import express from "express";
 
-const PORT =process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-const app=express()
+const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/api/message', (request, response)=>{
-  response.send('This is message sent from Vincent!');
-})
+app.get("/api/message", (request, response) => {
+  response.send("This is message sent from Vincent!");
+});
 
-app.get('/api/json', (_, res)=>{
-    res.json({
-        success: true,
-        author: 'vincent'
-    })
-})
+app.get("/api/json", (_, res) => {
+  res.json({
+    success: true,
+    author: "vincent",
+  });
+});
 
-app.listen(PORT, ()=>{
-    console.log(`Express is listening in port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Express is listening in port ${PORT}`);
+});
